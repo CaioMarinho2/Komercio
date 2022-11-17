9f30019aa057c5d689055325f5f9443ecfe715f8
Caio Marinho dos Reis <99349453+CaioMarinho2@users.noreply.github.com>
2022-11-17 03:24:16 -0300

    feat: identation

--- user/permissions.py
+++ user/permissions.py
@@ -1,7 +1,6 @@
 from rest_framework import permissions
 from rest_framework.views import Request, View
 
-
 class UsePatchRotePermition(permissions.BasePermission):
     def has_object_permission(self, request:Request, view:View, obj):
         return request.user.is_authenticated and obj.id==  request.user.id
